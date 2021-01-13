ebee5630a562f409a3393e02a8cce9dd0f7d5972
m1kurd <76977512+m1kurd@users.noreply.github.com>
2021-01-13 14:30:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -62,6 +62,6 @@ let reactionMenus = {};
 	// no reloading for statuses. statuses will be periodically fetched from mysql.
 	require("./modules/status.js")(passthrough)
 
-	client.login(Nzk3MDY1MjM4NDExMjE0ODQ4.X_hC2Q.g3dSZSc0terQVv6gds_LytrHTQ4);
+	client.login(Nzk3MDY1MjM4NDExMjE0ODQ4.X_hC2Q.ZwZd_3ZSLHFD_iUbWc58KmPU56o);
 
 })();
